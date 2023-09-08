16abedcd969daff326603805aaca7b1f798e1450
Yocxann BONI <yocxann.boni@gmail.com>
2023-09-08 18:14:33 +0200

    NavyDav

---
+++ src/Component/Navbar1.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+function Navbar1() {
+  return (
+    <div>Navbar1</div>
+  )
+}
+
+export default Navbar1
---
+++ src/Component/Navbar2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar2() {
+  return (
+    <div>Navbar2</div>
+  )
+}
+
+export default Navbar2
