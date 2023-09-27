1fcee0814ca5af681f74a5f42790d3932f20b046
ThomLand <thomsonbrisson210@gmail.com>
2023-09-27 15:06:38 +0200

    git1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Service from "./routes/service";
+
+function App() {
+  return (
+    <div className="App">
+      <Service />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/service.js
@@ -0,0 +1,54 @@
+import "../style/service.css";
+
+function Service() {
+  return (
+    <div className="page">
+      <div className="service">
+        <h1>Service 667</h1>
+      </div>
+
+      <div className="essentiel">
+        <h1>Essentiel</h1>
+      </div>
+
+      <div className="grand">
+        <div className="moyen">
+          <a href="">Carte</a>
+          <a href="">Joint</a>
+          <a href="">Pockets</a>
+          <a href="">Livret A</a>
+          <a href="">Compte lies</a>
+          <a href="">Parainage</a>
+        </div>
+      </div>
+
+      <div className="investissements">
+        <h1>Investissements</h1>
+      </div>
+      <div className="invgrand">
+        <div className="invmoyen">
+          <a href="">action</a>
+          <a href="">matiere premiere</a>
+          <a href="">crypto</a>
+          <a href="">cofre</a>
+          <a href="">epargne</a>
+        </div>
+      </div>
+
+      <div className="modedevie">
+        <h1>Mode de vie</h1>
+      </div>
+      <div className="modgrand">
+        <div className="modmoyen">
+          <a href="">sejour</a>
+          <a href="">Magasin</a>
+          <a href="">Dons</a>
+          <a href="">Cartes cadeaux</a>
+          <a href="">Experience</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Service;
